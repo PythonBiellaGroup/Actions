5750fc8af638c353223c9f61c27c9bb91754b2a2
Andrea Guzzo <andrea.guzzo92@gmail.com>
2023-03-01 16:55:02 +0100

    fixed docker-compose

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,6 +1,6 @@
-export PYTHONPATH=$(pwd)
-export API_ENDPOINT_PORT=8080
-export VERBOSITY=DEBUG
-export DEBUG_MODE=True
-python3 app/main.py
-#python3 app/main.py
+#Launch the application using gunicorn backend
+#gunicorn -w 4 -b 0.0.0.0:${APP_ENDPOINT_PORT:-8045} app.main:app
+#  --preload 
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:${API_ENDPOINT_PORT:-8000} --log-level ${LOG_VERBOSITY:-INFO} --timeout 300 app.main:app
+#--log-level ${APP_VERBOSITY:-INFO}
+#python ./app/main.py
